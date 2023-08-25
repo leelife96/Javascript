29a4a49e631bc8eaa4e30aca6dacd5f74b227672
leelife96 <leelife77@naver.com>
2023-08-25 21:46:38 +0900

    ds

---
+++ operation.js
@@ -0,0 +1,27 @@
+// Math operations
+
+console.log(10 + 4);
+console.log(10 - 4);
+console.log(10 * 4);
+console.log(10 / 4);
+console.log(10 % 4);
+console.log(10 % 3);
+console.log((10 + 3 - 5) * 10);
+
+// let result = (10 + 3 - 5) * 10;
+// result = 10 * 4;
+// result++; // result = result + 1
+// result--; // result = result - 1
+
+// result += 5; // result = result + 5
+// result -= 5; // result = result - 5
+// result /= 5; // result = result / 5
+// result *= 5; // result = result * 5
+// console.log(result);
+
+console.log('Max' +' ' + 'Schwarzmuller');
+console.log('Max' - 'ax');
+
+let username = 'Max';
+console.log(username.length);
+console.log(username.toUpperCase());
