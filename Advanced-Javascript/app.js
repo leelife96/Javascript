9dd2fd47c56ec85e50eb6d339839a9523f2b799f
leelife96 <leelife77@naver.com>
2023-09-02 21:46:55 +0900

    das

--- Advanced-Javascript/app.js
+++ Advanced-Javascript/app.js
@@ -1,1 +1,12 @@
-document.body.children[1].children[0].href = 'https://google.com';
+// document.body.children[1].children[0].href = 'https://google.com';
+
+let anchorElement = document.getElementById('external-link');
+anchorElement.href = 'https://google.com';
+
+anchorElement = document.querySelector('#external-id'); // a { color : red; }
+anchorElement.href = 'https://academind.com';
+
+let newAnchorElement = document.createElement('a');
+
+let fistParagraph = document.querySelector('p');
+fistParagraph.append(newAnchorElement);
