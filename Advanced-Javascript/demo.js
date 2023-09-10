70e3fd124127d2b3545090d493f7023dfba24fef
leelife96 <leelife77@naver.com>
2023-09-10 17:59:13 +0900

    d

---
+++ Advanced-Javascript/demo.js
@@ -0,0 +1,18 @@
+let productNameInputElement = document.getElementById('product-name');
+let remainingCharsElement = document.getElementById('remaining-chars');
+
+let maxAllowedChars = productNameInputElement.maxLength;
+
+// console.dir(productNameInputElement.maxLength);
+
+function updateRemainingCharacters(event) {
+    let enteredText = event.target.value;
+    let enteredTextLength = enteredText.length;  
+
+    let remainingCharacters = maxAllowedChars - enteredTextLength;
+
+    remainingCharsElement.textContent = remainingCharacters;
+}
+
+productNameInputElement.addEventListener('input', updateRemainingCharacters)
+
