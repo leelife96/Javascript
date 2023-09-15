a46c09e026f51c3959b06f1c27e195a760f684aa
leelife96 <leelife77@naver.com>
2023-09-15 22:30:40 +0900

    dadad

---
+++ Advanced-Javascript/conditional-code.js
@@ -0,0 +1,17 @@
+const myName = 'Max';
+
+if (myName === 'Max') {
+    console.log('Hello!');
+}
+
+let isLoggedIn = true;
+
+if (!isLoggedIn) {
+    console.log('User is NOT logged in!');
+}
+
+const enteredUserName = 'Maximilian';
+
+if (enteredUserName.length > 0) {
+    console.log('Input is valid!');
+}
