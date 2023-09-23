7aeb5af9672d11e6ae0f7e3c494848d83a1719b4
leelife96 <leelife77@naver.com>
2023-09-23 22:04:02 +0900

    das

--- Tik-Tac-Toe-Game/scripts/config.js
+++ Tik-Tac-Toe-Game/scripts/config.js
@@ -1,3 +1,9 @@
 function openPlayerConfig() {
-    
+    plyaerConfigOverlayElement.style.display = 'block';
+    backdropElement.style.display = 'block';
+}   
+
+function closePlayerConfig() {
+    plyaerConfigOverlayElement.style.display = 'none';
+    backdropElement.style.display = 'none';
 }
