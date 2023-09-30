b17a3509e30fa1a4d2aacde06324501093c13321
leelife96 <leelife77@naver.com>
2023-09-30 17:45:51 +0900

    d

--- Tik-Tac-Toe-Game/scripts/config.js
+++ Tik-Tac-Toe-Game/scripts/config.js
@@ -6,11 +6,18 @@ function openPlayerConfig() {
 function closePlayerConfig() {
     plyaerConfigOverlayElement.style.display = 'none';
     backdropElement.style.display = 'none';
+    formElement.firstElementChild.classList.remove('error'); 
+    errorsOutputElement.textContext = '';  
 }
 
 function savePlyaerConfig (event) {
     event.preventDefault();
     const formData = new FormData(event.target);
-    const enteredPlayername = formData.get('playername');
-    console.log(enteredPlayername);
+    const enteredPlayername = formData.get('playername').trim(); 
+
+    if(!enteredPlayername) {
+        event.target.firstElementChild.classList.add('error');
+        errorsOutputElement.textContext = 'Please enter a valid name!';
+        return;
+    }
 }
