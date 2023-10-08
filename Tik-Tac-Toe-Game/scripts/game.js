85fc7b00e171d50b28645a9046f80878de69a0c3
leelife96 <leelife77@naver.com>
2023-10-08 20:58:39 +0900

    d

--- Tik-Tac-Toe-Game/scripts/game.js
+++ Tik-Tac-Toe-Game/scripts/game.js
@@ -3,5 +3,21 @@ function startNewGame() {
         alert('Please set custom plyaer names for both players!');
         return; 
     }
+    activePlayerNameElement.textContent = players[activePlayer].name;    
     gameAreaElement.style.display = 'block';
 }
+
+function switchPlayer() {
+    if(activePlayer === 0) {
+        activePlayer = 1;
+    } else {
+        activePlayer = 0;
+    }
+    activePlayerNameElement.textContent = players[activePlayer].name;
+}
+
+function selectGameField(event) {
+    event.target.textContent = players[activePlayer].symbol;
+    event.target.classList.add('disabled'); 
+    switchPlayer();
+} 
